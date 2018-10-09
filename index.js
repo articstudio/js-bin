c1c9b1290e470849a7f94f7f13ebf45187c65479
Marc Mascort Bou <marc@articstudio.com>
2018-10-09 10:16:22 +0200

    Webpack config

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = (function () {
     'use strict';
 
+    let sminkon = require('pigmento-sminkon-config'),
+            path = require('path');
+
+    // Config Sminkon
+    sminkon.addEntry('loading', path.resolve(__dirname, './resources/js/loading.js'));
+    sminkon.setTemplate(path.resolve(__dirname, './resources/public/index.ejs'));
+
     var exports = {
 
     };
