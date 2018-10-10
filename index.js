df5b2c2fc6ab4e776c6a6c7e1e9ab99fcd095b27
Mauro Balestra <mbalestra94@gmail.com>
2018-10-10 12:40:36 +0200

    Loader funcionant, configuracions afegides (sminkon-config)

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ module.exports = (function () {
         path = require('path');
 
     // Config Sminkon
-    sminkon.addEntry('loading', [
-        path.resolve(__dirname, './resources/js/loading.js'),
-        path.resolve(__dirname, './resources/sass/loading.scss')
-    ]);
+    sminkon.addEntry(
+        'loading', [
+            path.resolve(__dirname, './resources/js/loading.js'),
+            path.resolve(__dirname, './resources/sass/loading.scss')
+        ]);
+    sminkon.addEntry(
+        'main', [
+            path.resolve(__dirname, './resources/js/main.js'),
+        ]);
     sminkon.setTemplate(path.resolve(__dirname, './resources/public/index.ejs'));
 
-    //sminkon.setAssets('loading_js', '/js/loading.js');
-    //sminkon.setAssets('loading_css', '/css/loading.css');
+    sminkon.setAssets('images', path.resolve(__dirname, './resources/images'));
+    sminkon.setAssets('i18n', path.resolve(__dirname, './resources/i18n'));
+    sminkon.setAssets('', path.resolve(__dirname, './resources/public'));
 
     var exports = {};
 
