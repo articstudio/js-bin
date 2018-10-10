8b35cb45b617fab62336f0070abb4e44e722ee2a
Marc Mascort Bou <marc@articstudio.com>
2018-10-10 17:23:03 +0200

    Use sminkon utils

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 module.exports = (function () {
     'use strict';
 
-    let sminkon = require('pigmento-sminkon-config'),
+    let utils = require('pigmento-sminkon-utils'),
         path = require('path');
 
     // Config Sminkon
-    sminkon.addEntry(
+    utils.config.addEntry(
         'loading', [
             path.resolve(__dirname, './resources/js/loading.js'),
             path.resolve(__dirname, './resources/sass/loading.scss')
         ]);
-    sminkon.addEntry(
+    utils.config.addEntry(
         'main', [
             path.resolve(__dirname, './resources/js/main.js'),
         ]);
-    sminkon.setTemplate(path.resolve(__dirname, './resources/public/index.ejs'));
+    utils.config.setTemplate(path.resolve(__dirname, './resources/public/index.ejs'));
 
-    sminkon.setAssets('images', path.resolve(__dirname, './resources/images'));
-    sminkon.setAssets('i18n', path.resolve(__dirname, './resources/i18n'));
-    sminkon.setAssets('', path.resolve(__dirname, './resources/public'));
+    utils.config.setAssets('images', path.resolve(__dirname, './resources/images'));
+    utils.config.setAssets('i18n', path.resolve(__dirname, './resources/i18n'));
+    utils.config.setAssets('', path.resolve(__dirname, './resources/public'));
 
     var exports = {};
 
