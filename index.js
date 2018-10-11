570d731cb259ca166bab2d9d310bd918f471e4fd
Mauro Balestra <mbalestra94@gmail.com>
2018-10-11 14:10:50 +0200

    Mogut loader del core cap al utils

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ module.exports = (function () {
 
     let exports = {
         config: require('./src/config.js'),
-        base64: require('./src/base64.js')
+        base64: require('./src/base64.js'),
+        loader: require('./src/loader')
     };
+
+    exports.config.setAlias('fs', 'mime/mime');
 
     return exports;
 })();
