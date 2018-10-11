b3b3130d731f801910d9afaef6047033fdad62de
Mauro Balestra <mbalestra94@gmail.com>
2018-10-11 14:10:11 +0200

    Refactor de la configuració de webpack, més dinàmica

--- src/config.js
+++ src/config.js
@@ -4,12 +4,15 @@ module.exports = (function () {
     let config = {
         version: null,
         hash: function () {
-                    return new Date().getTime();
-                },
+            return new Date().getTime();
+        },
+        output: null,
         assets: {},
         entry: {},
-        alias: [],
+        alias: {},
+        plugins: [],
         images: [],
+        rules: [],
         template: null
     };
 
@@ -19,6 +22,13 @@ module.exports = (function () {
         },
         getTemplate: function () {
             return config.template;
+        },
+        //
+        setOutput: function (value) {
+            config.output = value
+        },
+        getOutput: function () {
+            return config.output;
         },
         //
         addEntry: function (key, value) {
@@ -40,21 +50,50 @@ module.exports = (function () {
         setVersion: function (value) {
             config.version = value;
         },
+        //
         setAsset: function (key, value) {
             config.assets[key] = value;
         },
-        getAsset: function(key) {
+        getAsset: function (key) {
             return config.assets[key];
         },
-        getAssets: function(){
+        getAssets: function () {
             return config.assets;
+        },
+        //
+        setAlias: function (key, value) {
+            config.alias[key] = value;
+        },
+        getAlias: function (key) {
+            return config.alias[key];
+        },
+        getAliases: function () {
+            return config.alias;
+        },
+        //
+        setRule: function (value) {
+            config.rules.push(value);
+        },
+        getRules: function () {
+            return config.rules;
+        },
+        //
+        setPlugin: function (value) {
+            config.plugins.push(value)
+        },
+        getPlugins: function () {
+            return config.plugins;
+        },
+        //
+        isProduction: function(){
+            return !!(process.env.NODE_ENV === 'production');
         },
         //
         getConfig: function () {
             return {
                 hash: config.hash,
                 images: config.images,
-                develop: !!(process.env.NODE_ENV !== 'production'),
+                develop: !this.isProduction(),
                 version: config.version
             };
         }
