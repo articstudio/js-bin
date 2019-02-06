dde03c29f519f7b5d68c4928e5a838d0aaaa70da
Mauro Balestra <mbalestra94@gmail.com>
2019-02-06 11:50:10 +0100

    Instantiate a Application

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,6 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const clear = require('clear');
+const App = require('../Application');
 
-const test_command = require('../src/Commands/test');
-const Menu = require('../src/Commands/Menu');
-
-
-clear();
-
-
-if(process.argv.length <= 2){
-    Menu();
-}
-
-program
-    .version('1.0.0', '-v, --version')
-    .command('*')
-    .description('No arguments')
-    .action(function (cmd) {
-        console.error("Invalid argument: ", cmd);
-    });
-
-program
-    .command('test')
-    .alias('t')
-    .description('Test command')
-    .action(function () {
-        test_command();
-    });
-
-program.parse(process.argv);
-
-
-
+new App().exec();
 
