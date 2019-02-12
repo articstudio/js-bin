54e5035a088ad7c323ffbbc72ac696c7bc1f6d3d
Mauro Balestra <mbalestra94@gmail.com>
2019-02-12 11:58:45 +0100

    Change format to pick subtrees, and testing exec node.

--- src/Commands/Git/utils/SubstreesConfig.js
+++ src/Commands/Git/utils/SubstreesConfig.js
@@ -1,10 +1,32 @@
 const app = require('../../../Application');
+const { exec } = require('child_process');
 
 module.exports = {
     getSubtrees: function () {
         let package_json = app.getPackage();
         let config = package_json.hasOwnProperty('data') && package_json.data.hasOwnProperty('config') ? package_json.data.config : [];
 
-        return config.hasOwnProperty('subtree') ? config.subtree : [];
+        if (!config.hasOwnProperty('subtree')) {
+            return [];
+        }
+
+        return Object.keys(config.subtree).map(key => {
+            return {
+                name: key,
+                url: config.subtree[key]
+            }
+        });
     },
+    getLocalChanges: function () {
+        exec('git diff', ['--exit-code'], (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    },
+    commitChanges: function (message, files) {
+
+    }
 };
