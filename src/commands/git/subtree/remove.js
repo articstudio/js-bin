ef1cb67225a0b5024b0db9e972fb00c24f27aa0f
Marc Mascort Bou <marc@articstudio.com>
2019-02-27 02:54:56 +0100

    WIP

--- src/commands/git/subtree/remove.js
+++ src/commands/git/subtree/remove.js
@@ -60,7 +60,7 @@ let constructor = function (app) {
                             if (!app.utils.git.existsSubtree(data[0])) {
                                 return;
                             }
-                            let done = app.utils.git.pullSubtree(data[0], data[1], !verbosity);
+                            let done = app.utils.git.removeSubtree(data[0], data[1], !verbosity);
                             if (!verbosity) {
                                 done ? app.utils.ui.success('- ' + data[0]) : app.utils.ui.error('! ' + data[0]);
                             }
