65dd7685dd905ee5c8ddd48018d52b89c589dfd1
Marc Mascort Bou <marc@articstudio.com>
2019-03-20 20:38:53 +0100

    Npm package check support

---
+++ src/commands/package/check.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let constructor = function (app) {
+    return app.abstracts.command.extend({
+        name: 'package:check',
+        description: 'Package Check',
+        options: [['-s, --save', 'Save in package.json']],
+        action: function (cmd) {
+            let save = cmd.save || false;
+            let file = app.utils.path.resolve(app.getPath(), './package.json');
+            this.exec('./node_modules/.bin/ncu --packageFile ' + file + (save ? ' --upgrade' : ''));
+        },
+    });
+};
+
+module.exports = constructor;
